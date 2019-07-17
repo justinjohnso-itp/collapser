a6198c4cef4cb96e9bf552a85de72fff49248967
Aaron Reed <aareed@gmail.com>
2019-07-17 15:10:21 -0700

    Added reading in input file

--- collapser.py
+++ collapser.py
@@ -1,5 +1,36 @@
 #!/usr/bin/python
 
-print """Collapser 0.1
-Usage: collapser <INPUT> <OUTPUT> options
-"""
+import sys
+
+inputFile = ""
+outputFile = ""
+inputText = ""
+outputText = ""
+
+def showUsage():
+	print """Usage: collapser <INPUT> <OUTPUT> options"""
+
+
+def readInputFile(inputFile):
+	fileContents = ""
+	with open(inputFile, "r") as fileObject:
+		return fileObject.read()
+	print "Can't read '%s'." % inputFile
+	sys.exit()
+
+
+
+print """Collapser 0.1"""
+
+if len(sys.argv) != 3:
+	showUsage()
+	sys.exit()
+
+
+
+inputFile = sys.argv[1]
+outputFile = sys.argv[2]
+
+inputText = readInputFile(inputFile)
+
+print "Here is the file we read: '%s'" % inputText
