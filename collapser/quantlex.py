9474c5753079569a92715eb5e1aa96606f94b986
Aaron Reed <aareed@gmail.com>
2019-07-26 15:48:58 -0700

    Add print function for LexerResult

--- collapser/quantlex.py
+++ collapser/quantlex.py
@@ -139,6 +139,14 @@ class LexerResult:
 		self.errorLineText = ""
 		self.errorMessage = ""
 
+	def __str__(self):
+		if self.isValid == False:
+			return "INVALID: Line %d Col %d %s (%s)" % (self.errorLineNumber, self.errorColumn, self.errorMessage, self.errorLineText)
+		else:
+			output = ""
+			for token in self.tokens:
+				output += token + ", "
+			return output
 
 def lex(text):
 	resetLexState()
