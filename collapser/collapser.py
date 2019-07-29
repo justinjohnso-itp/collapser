847849d027189d3e15d8822baf702c21c528e394
Aaron Reed <aareed@gmail.com>
2019-07-28 19:47:26 -0700

    Add stub for sanity checker

--- collapser/collapser.py
+++ collapser/collapser.py
@@ -15,6 +15,10 @@ def showUsage():
 	print """Usage: collapser <INPUT> <OUTPUT> options"""
 
 
+def sanityCheck(text):
+	# Look for unexpected characters etc. here
+	return
+
 def main():
 
 	print """Collapser 0.1"""
@@ -33,6 +37,8 @@ def main():
 	collapsedText = collapse.go(inputText)
 
 	outputText = latexifier.go(collapsedText)
+
+	sanityCheck(outputText)
 
 	# print "\n\nHere is the output:\n%s" % outputText
 
