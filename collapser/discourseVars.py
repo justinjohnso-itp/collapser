e372cfa7a23834bc56b23511790eb1c90a189f7d
Aaron Reed <aareed@gmail.com>
2019-09-16 15:04:32 -0700

    Improve detecting be verbs

--- collapser/discourseVars.py
+++ collapser/discourseVars.py
@@ -2,19 +2,24 @@
 
 import re
 
-beVerbsRegex = re.compile(r"\b(be|been|being|am|is|isn't|are|aren't|was|wasn't|were|weren't|i'm|he's|she's|it's|they're|you're|that's|there's|what's)\b")
-smartQuoteRegex = re.compile(r"[‘’]")
-
 def getDiscoursePreferredVersion(alts):
 	# For each discourse variable set, rank each alt for desireability. Return something weighted for the highest-ranked options.
 	# TODO only within quoted dialogue.
 	dpQuality = []
 	for pos, item in enumerate(alts):
-		thisItem = re.sub(smartQuoteRegex, "'", item)  # ignore smart quotes
 		if variables.set("avoidbe"):
 			# Subtract one for each "be" verb.
-			score = 1 - getNumBeVerbsInText(thisItem)
+			score = 1 - getNumBeVerbsInText(item)
 			dpQuality[pos] = score
 
+
+
+
+
+beVerbsRegex = re.compile(r"\b(be|been|being|am|is|isn't|are|aren't|was|wasn't|were|weren't|i'm|he's|she's|it's|they're|you're|that's|there's|what's)\b", re.IGNORECASE)
+
 def getNumBeVerbsInText(txt):
+	# Fix smart quotes
+	txt = txt.replace("‘", "'")
+	txt = txt.replace("’", "'")
 	return len(re.findall(beVerbsRegex, txt))
--- collapser/test_discourseVars.py
+++ collapser/test_discourseVars.py
@@ -1,7 +1,22 @@
+# coding=utf-8
 
 import discourseVars
 import pytest
 
 def test_getNumBeVerbsInText():
+	text = '''Absolutely none of those nasty verbs here.'''
+	assert discourseVars.getNumBeVerbsInText(text) == 0
+	text = '''A bee or an island don't count.'''
+	assert discourseVars.getNumBeVerbsInText(text) == 0
+	text = '''be at the start: okay.'''
+	assert discourseVars.getNumBeVerbsInText(text) == 1
+	text = '''A capital Be also counts'''
+	assert discourseVars.getNumBeVerbsInText(text) == 1
 	text = '''To be or not to be.'''
 	assert discourseVars.getNumBeVerbsInText(text) == 2
+	text = '''I wasn’t going to care about smart quotes.'''
+	assert discourseVars.getNumBeVerbsInText(text) == 1
+	text = '''I wasn't going to be angry, but they're being gross.'''
+	assert discourseVars.getNumBeVerbsInText(text) == 4
+	text = '''Punctuation:be?Was:were,isn't.'''
+	assert discourseVars.getNumBeVerbsInText(text) == 4
