e894fd7c36b0e24ed3a8b4462987ccc2f32e9290
Aaron Reed <aareed@gmail.com>
2019-09-17 22:39:52 -0700

    Reorder functions in latexifier

--- collapser/latexifier.py
+++ collapser/latexifier.py
@@ -2,7 +2,7 @@
 
 # This handles converting a collapsed text into a properly formated Latex file.
 
-# We're looking for curly-brace things like {chapter|2} and replacing them with the appropriate latex. Also more basic things like *bold*.
+# We're looking for curly-brace things like {chapter|2} and replacing them with the appropriate latex. 
 
 import re
 
@@ -13,6 +13,112 @@ def go(sourceText, templates, doFront):
 	postConversionSanityCheck(output)
 	output = latexWrapper(output, templates, includeFrontMatter=doFront)	
 	return output
+
+
+def specialFixes(text):
+	# Strip file identifiers (used by the lexer and parser to know what source file a given line comes from, so useful error messages can be printed).
+	text = re.sub(r"\% file (.*)\n", "", text)
+
+	# Ensure verses don't break across pages.
+	# {verse/A looking-glass held above this stream...}
+	text = re.sub(r"{verse/(.*)}", r"{verse/\g<1> \\nowidow }", text)
+
+	# Ensure no widows right before chapter breaks.
+	text = re.sub(r"([\n\s]*){chapter/", r" \\nowidow \g<1>{chapter/", text)
+
+	# Ensure no orphans right after section breaks.
+	text = re.sub(r"{section_break}(\n*)(.*)\n", r"{section_break}\g<1>\g<2> \\noclub \n", text)
+
+	# Use proper latex elipses
+	text = re.sub(r"\.\.\. ", r"\ldots\ ", text)
+	text = re.sub(r"… ", r"\ldots\ ", text)
+
+
+	return text
+
+
+def renderControlSeqs(sourceText):
+	rendered = []
+	pos = 0
+
+
+	formatStartPos = sourceText.find("{", pos)
+	while formatStartPos is not -1:
+		rendered.append(sourceText[pos:formatStartPos])
+		formatEndPos = sourceText.find("}", formatStartPos)
+		if formatEndPos is -1:
+			raise ValueError("Found { without closing brace.")
+		codeSeq = sourceText[formatStartPos:formatEndPos+1]
+		contents = codeSeq[1:-1].split('/')
+		code = contents[0]
+		repl = ""
+
+		if code == "part":
+			partNum = contents[1]
+			partTitle = contents[2]
+			epigraph = contents[3]
+			source = contents[4]
+			repl = template_part[0] + partNum + template_part[1] + partTitle + template_part[2] + epigraph + template_part[3] + source + template_part[4]
+		elif code == "chapter":
+			chapNum = contents[1]
+			repl = template_chapter[0] + chapNum + template_chapter[1]
+		elif code == "section_break":
+			repl = template_section_break
+		elif code == "verse":
+			text = contents[1]
+			repl = template_verse[0] + text + template_verse[1]
+		elif code == "verse_inline":
+			text = contents[1]
+			repl = template_verse_inline[0] + text + template_verse_inline[1]
+		elif code == "pp":
+			repl = template_pp
+		elif code == "i":
+			text = contents[1]
+			repl = template_i[0] + text + template_i[1]
+		elif code == "vspace":
+			text = contents[1]
+			repl = template_vspace[0] + text + template_vspace[1]
+
+		elif code == "test":
+			repl = "<<TEST_STANDALONE>>"
+		elif code == "test_param":
+			repl = "<<TEST_PARAMS>>" + contents[1] + "<<END>>"
+
+		else:
+			raise ValueError("Unrecognized command '%s' in control sequence '%s'" % (code, codeSeq)) 
+
+		rendered.append(repl)
+		pos = formatEndPos+1
+		formatStartPos = sourceText.find("{", pos)
+
+	rendered.append(sourceText[pos:len(sourceText)])
+
+	return ''.join(rendered)	
+
+
+def postConversionSanityCheck(text):
+	# Look for unexpected characters etc. here
+	pos = text.find('_')
+	if pos is not -1:
+		raise ValueError("Found invalid underscore '_' character on line %d:\n%s" % (result.find_line_number(text, pos), result.find_line_text(text, pos)) )
+	
+	# pos = text.find('''"''')
+	# if pos is not -1:
+	# 	raise ValueError("Found dumb quote character on line %d; use “ ” \n%s" % (quantlex.find_line_number(text, pos), quantlex.find_line_text(text, pos)) )
+
+	return
+
+
+def latexWrapper(text, templates, includeFrontMatter=True):
+	output = templates["begin"]
+	if includeFrontMatter:
+		output += templates["frontMatter"]
+	output += templates["postFrontMatter"]
+	output += text
+	output += templates["end"]
+	return output
+
+
 
 
 
@@ -87,110 +193,5 @@ template_vspace = ['''
 
 
 
-def latexWrapper(text, templates, includeFrontMatter=True):
-	output = templates["begin"]
-	if includeFrontMatter:
-		output += templates["frontMatter"]
-	output += templates["postFrontMatter"]
-	output += text
-	output += templates["end"]
-	return output
-
-
-
-# Pre-latexifier.
-def postConversionSanityCheck(text):
-	# Look for unexpected characters etc. here
-	pos = text.find('_')
-	if pos is not -1:
-		raise ValueError("Found invalid underscore '_' character on line %d:\n%s" % (result.find_line_number(text, pos), result.find_line_text(text, pos)) )
-	
-	# pos = text.find('''"''')
-	# if pos is not -1:
-	# 	raise ValueError("Found dumb quote character on line %d; use “ ” \n%s" % (quantlex.find_line_number(text, pos), quantlex.find_line_text(text, pos)) )
-
-	return
-
-
-def renderControlSeqs(sourceText):
-	rendered = []
-	pos = 0
-
-
-	formatStartPos = sourceText.find("{", pos)
-	while formatStartPos is not -1:
-		rendered.append(sourceText[pos:formatStartPos])
-		formatEndPos = sourceText.find("}", formatStartPos)
-		if formatEndPos is -1:
-			raise ValueError("Found { without closing brace.")
-		codeSeq = sourceText[formatStartPos:formatEndPos+1]
-		contents = codeSeq[1:-1].split('/')
-		code = contents[0]
-		repl = ""
-
-		if code == "part":
-			partNum = contents[1]
-			partTitle = contents[2]
-			epigraph = contents[3]
-			source = contents[4]
-			repl = template_part[0] + partNum + template_part[1] + partTitle + template_part[2] + epigraph + template_part[3] + source + template_part[4]
-		elif code == "chapter":
-			chapNum = contents[1]
-			repl = template_chapter[0] + chapNum + template_chapter[1]
-		elif code == "section_break":
-			repl = template_section_break
-		elif code == "verse":
-			text = contents[1]
-			repl = template_verse[0] + text + template_verse[1]
-		elif code == "verse_inline":
-			text = contents[1]
-			repl = template_verse_inline[0] + text + template_verse_inline[1]
-		elif code == "pp":
-			repl = template_pp
-		elif code == "i":
-			text = contents[1]
-			repl = template_i[0] + text + template_i[1]
-		elif code == "vspace":
-			text = contents[1]
-			repl = template_vspace[0] + text + template_vspace[1]
-
-		elif code == "test":
-			repl = "<<TEST_STANDALONE>>"
-		elif code == "test_param":
-			repl = "<<TEST_PARAMS>>" + contents[1] + "<<END>>"
-
-		else:
-			raise ValueError("Unrecognized command '%s' in control sequence '%s'" % (code, codeSeq)) 
-
-		rendered.append(repl)
-		pos = formatEndPos+1
-		formatStartPos = sourceText.find("{", pos)
-
-	rendered.append(sourceText[pos:len(sourceText)])
-
-	return ''.join(rendered)	
-
-
-
-def specialFixes(text):
-	# Strip file identifiers (used by the lexer and parser to know what source file a given line comes from, so useful error messages can be printed).
-	text = re.sub(r"\% file (.*)\n", "", text)
-
-	# Ensure verses don't break across pages.
-	# {verse/A looking-glass held above this stream...}
-	text = re.sub(r"{verse/(.*)}", r"{verse/\g<1> \\nowidow }", text)
-
-	# Ensure no widows right before chapter breaks.
-	text = re.sub(r"([\n\s]*){chapter/", r" \\nowidow \g<1>{chapter/", text)
-
-	# Ensure no orphans right after section breaks.
-	text = re.sub(r"{section_break}(\n*)(.*)\n", r"{section_break}\g<1>\g<2> \\noclub \n", text)
-
-	# Use proper latex elipses
-	text = re.sub(r"\.\.\. ", r"\ldots\ ", text)
-	text = re.sub(r"… ", r"\ldots\ ", text)
-
-
-	return text
 
 
