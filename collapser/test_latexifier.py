948d10dde978d6cedfa73bb0f8ef936d35ae7dc0
Aaron Reed <aareed@gmail.com>
2019-09-17 23:08:42 -0700

    Fix latexifier unit tests

--- collapser/test_latexifier.py
+++ collapser/test_latexifier.py
@@ -4,18 +4,18 @@ import pytest
 
 def test_stringSwap():
 	text = "This is a test of {test} standalone."
-	result = latexifier.go(text)
+	result = latexifier.renderControlSeqs(text)
 	assert result == "This is a test of <<TEST_STANDALONE>> standalone."
 
 	text = "{test}{test}"
-	result = latexifier.go(text)
+	result = latexifier.renderControlSeqs(text)
 	assert result == "<<TEST_STANDALONE>><<TEST_STANDALONE>>"
 
 	text = "This is my {test_param/alpha}"
-	result = latexifier.go(text)
+	result = latexifier.renderControlSeqs(text)
 	assert result == "This is my <<TEST_PARAMS>>alpha<<END>>"
 
 def test_formatting():
 	text = "This is {i/italic}."
-	result = latexifier.go(text)
+	result = latexifier.renderControlSeqs(text)
 	assert result == "This is \\textit{italic}."
