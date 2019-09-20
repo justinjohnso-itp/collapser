f3993bdaba551a1a73460bc26b64a69a335b49be
Aaron Reed <aareed@gmail.com>
2019-09-19 18:37:43 -0700

    Additional renderAll tests

--- collapser/test_confirm.py
+++ collapser/test_confirm.py
@@ -31,12 +31,28 @@ def getFirstCtrlSeq(tokens):
 		index += 1
 	return []
 
+def parseAndGetAlts(text):
+	tokens = parseResult(text)
+	ctrlseq = getFirstCtrlSeq(tokens)
+	return variables.renderAll(ctrlseq)
 
 def test_renderAllVariables():
 	text = "[DEFINE @alpha1]This is some text. [@alpha1>Variant the first|Version the second]. And some final text."
-	tokens = parseResult(text)
-	ctrlseq = getFirstCtrlSeq(tokens)
-	alts = variables.renderAll(ctrlseq)
-	print "alts: %s" % alts
+	alts = parseAndGetAlts(text)
 	assert alts[0].txt == "Variant the first"
 	assert alts[1].txt == "Version the second"
+
+	text = "[DEFINE @beta]Text. [@beta>Only if beta.] End."
+	alts = parseAndGetAlts(text)
+	assert alts[0].txt == "Only if beta."
+	assert alts[1].txt == ""
+
+	text = "[DEFINE @gamma|@delta]Text. [@gamma>Gamma forever|@delta>This is delta|Or not] End."
+	alts = parseAndGetAlts(text)
+	assert alts[0].txt == "Gamma forever"
+	assert alts[1].txt == "This is delta"
+	assert alts[2].txt == "Or not"
+
+
+
+
