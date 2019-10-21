5b4377db4f9b10213f89684160e9ddfdb8b2f659
Aaron Reed <aareed@gmail.com>
2019-10-21 15:19:20 -0700

    Tweaks to output style of text renderer

--- collapser/renderer_text.py
+++ collapser/renderer_text.py
@@ -16,6 +16,7 @@ class RendererText(renderer.Renderer):
 		pass
 
 	def makeOutputFile(self):
+		self.collapsedText = prepForTextOutput(self.collapsedText)
 		workFile = self.renderControlSequences()
 		workFile = specialTextFixes(workFile)
 		postTextificationSanityCheck(workFile)
@@ -29,7 +30,7 @@ class RendererText(renderer.Renderer):
 			partTitle = contents[2]
 			epigraph = contents[3]
 			source = contents[4]
-			return "\n\n\n" + partNum + "\n\n" + partTitle + "\n\n\n" + epigraph + "\n\n" + source + "\n\n\n\n"
+			return "\n\n\n\n\n" + partNum + "\n\n" + partTitle + "\n\n\n\n" + epigraph + "\n\n" + source + "\n\n"
 		if code == "epigraph":
 			epigraph = contents[1]
 			source = contents[2]
@@ -38,19 +39,18 @@ class RendererText(renderer.Renderer):
 			chapNum = contents[1]
 			return "\n\n\n\nChapter " + chapNum + "\n\n\n"
 		if code == "section_break":
-			return "\n\n         * * * * * * * * * * * * * * * * * * * * * * * * * *\n\n"
+			return "\n#\n"
 		if code == "verse":
-			text = contents[1]
-			# TODO need to indent lines
+			text = indent(contents[1])
 			return "\n\n" + text + "\n\n"
 		if code == "verse_inline":
-			text = contents[1]
+			text = indent(contents[1])
 			return "\n    " + text + "\n"
 		if code == "pp":
 			return "\n\n"
 		if code == "i":
 			text = contents[1]
-			return "/" + text + "/"
+			return "_" + text + "_"
 		if code == "vspace":
 			# TODO: Make this work if there's a need.
 			return "\n\n\n"
@@ -58,11 +58,41 @@ class RendererText(renderer.Renderer):
 		raise ValueError("Unrecognized command '%s' in control sequence '%s'" % (code, contents)) 
 
 
+def indent(text):
+	lines = text.split('\n')
+	lines_indented = map(lambda line: "    " + line, lines)
+	return '\n'.join(lines_indented)
+
+def prepForTextOutput(text):
+	# Fixes that need to happen before we expand formatting sequences.
+
+	# Get rid of extra whitespace on lines.
+	text = re.sub(r"\n[ \t]*\n", "\n\n", text)
+
+	# Collapse multiple line breaks in a row (before formatter adds any)
+	text = re.sub(r"[\n]{3,}", "\n\n", text)
+
+	# Remove space at start of lines (before formatter adds any)
+	text = re.sub(r"\n[ \t]*", "\n", text)
+
+	return text
+
 def specialTextFixes(text):
+	# Fixes that should happen after all output is rendered.
+	# Fix unicode quotes and special chars
 	text = re.sub(r"’", "'", text)
 	text = re.sub(r"‘", "'", text)
 	text = re.sub(r"“", '"', text)
 	text = re.sub(r"”", '"', text)
+	text = re.sub(r"…", "...", text)
+	text = re.sub(r"—", "---", text)
+
+	# Remove Latex explicit line break markers
+	text = re.sub(r"\\\\", "", text)
+
+	# Fix single spaces at start of new lines (we can't get rid of these earlier because we might have a tag like {pp} we haven't processed yet, but we only look for single spaces to avoid removing epigraph indents.)
+	text = re.sub(r"\n (\w)", r"\n\1", text)
+
 	return text
 
 def postTextificationSanityCheck(text):
