7791294a6a41171fdd63e2b30412447755d2fa1a
Aaron Reed <aareed@gmail.com>
2019-12-11 22:07:25 -0800

    Reorganize token_stream

--- collapser/token_stream.py
+++ collapser/token_stream.py
@@ -2,42 +2,7 @@
 import result
 
 
-class SequenceStream:
-
-	def __init__(self, tokens):
-		self.reset()
-		self.sequences = []
-		self.parseCtrlSeqs(tokens)
-
-	def parseCtrlSeqs(self, tokens):
-		ts = TokenStream(tokens)
-		nextBit = ts.next()
-		while nextBit is not None:
-			if type(nextBit) != str:
-				self.sequences.append([nextBit, ts.lastLexPos])
-			nextBit = ts.next()
-
-	def reset(self):
-		self.pos = 0
-
-	def preceding(self):
-		if self.pos <= 0:
-			return [None, None]
-		return self.sequences[self.pos-1]
-	
-	def next(self):
-		if self.pos >= len(self.sequences):
-			return [None, None]
-		nextSeq = self.sequences[self.pos]
-		self.pos += 1
-		return nextSeq
-
-	def following(self):
-		if self.pos >= len(self.sequences) - 1:
-			return [None, None]
-		return self.sequences[self.pos+1]
-
-
+# Abstraction over an array of parsed tokens, which will be text or control sequences.
 
 class TokenStream:
 
@@ -78,5 +43,43 @@ class TokenStream:
 		if nextBit is None:
 			return None
 		return nextBit
+
+
+# Abstraction over an array of parsed tokens when we only care about the control sequences, including the ability to get the ones before and after the current one we're considering.
+
+class SequenceStream:
+
+	def __init__(self, tokens):
+		self.reset()
+		self.sequences = []
+		self.parseCtrlSeqs(tokens)
+
+	def parseCtrlSeqs(self, tokens):
+		ts = TokenStream(tokens)
+		nextBit = ts.next()
+		while nextBit is not None:
+			if type(nextBit) != str:
+				self.sequences.append([nextBit, ts.lastLexPos])
+			nextBit = ts.next()
+
+	def reset(self):
+		self.pos = 0
+
+	def preceding(self):
+		if self.pos <= 0:
+			return [None, None]
+		return self.sequences[self.pos-1]
+	
+	def next(self):
+		if self.pos >= len(self.sequences):
+			return [None, None]
+		nextSeq = self.sequences[self.pos]
+		self.pos += 1
+		return nextSeq
+
+	def following(self):
+		if self.pos >= len(self.sequences) - 1:
+			return [None, None]
+		return self.sequences[self.pos+1]
 
 
