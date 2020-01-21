f6e97941dcc7217d41532d55fea953cf727c5b53
Aaron Reed <aareed@gmail.com>
2020-01-20 19:48:35 -0800

    Additional excerpt choices

--- collapser/renderer.py
+++ collapser/renderer.py
@@ -130,6 +130,15 @@ def getSequenceToRender(parseParams):
 	choices["Ch1IntroScene"] = ["Ch1IntroScene", 1, "Right from the start things were wrong, but I couldn’t see it. Maybe I didn’t want to. Or maybe I’m being too hard on myself. There wasn’t exactly a roadmap for what happened, a script to follow. But it’s undeniable that even on that very first night---the night of the Russian dance club, remember?---everything was already wrong."]
 	choices["Ch3PartyConvo"] = ["Ch3PartyConvo", 3, "We listened to the music for a minute, surrounded by people who naturally knew how to Saturday night, without training. It was kind of nice being near them, at least."]
 	choices["Ch4Interlude"] = ["Ch4Interlude", 4, "I worked up my courage and did a few of my own solo expeditions Downstairs, without telling him, but I couldn't convince myself to go very far. I hallucinated strange noises around corners: floorboards creaking, whispered sighs. I knew I was only scaring myself, but didn’t have it in me to stay down there for long."]
+	choices["Ch6ResearchResults"] = ["Ch6ResearchResults", 6, "It was a blurry copy of an old newspaper from around the turn of the century. I didn't see the relevance at first until I noticed an address, and almost choked on my tea."]
+	choices["Ch8SpiralHall"] = ["Ch8SpiralHall", 8, "It was subtle at first. But the horizontal hallways were becoming less and less level. We'd stumble on a floor that canted slightly left, or tilted a half-degree up or down. The walls, too, were growing angled, some leaning outward a degree or two instead of staying neatly parallel, or bent slightly inward at mismatched angles. It made us feel drunk. You've seen so many well-constructed hallways in your life, your brain doesn't know how to process ones that don't behave."]
+	choices["Ch8HamsterWheel"] = ["Ch8HamsterWheel", 8, "Some of the rooms got larger, too big for rooms in a house. More like a school gymnasium. Still the same carpet, though. And it felt like we were seeing more of the anomalies, the farther in and deeper down we got. An explosion of pipes and plumbing, sticking out of a wall for no particular reason; weird cube-shaped extrusions or cavities in the edges of rooms. It was like the deeper we went, the more flexible the rules became—of architecture, of stability, of god knows what else."]
+	choices["Ch8StageLadder"] = ["Ch8StageLadder", 8, "Some of the rooms got larger, too big for rooms in a house. More like a school gymnasium. Still the same carpet, though. And it felt like we were seeing more of the anomalies, the farther in and deeper down we got. An explosion of pipes and plumbing, sticking out of a wall for no particular reason; weird cube-shaped extrusions or cavities in the edges of rooms. It was like the deeper we went, the more flexible the rules became—of architecture, of stability, of god knows what else."]
+	choices["Ch11TheBottom"] = ["Ch11TheBottom", 11, "He turned back to me again, no longer wistful but with dangerous sharpness. Maybe you've heard the phrase “thousand-yard stare” and maybe you've even seen one before, but I hadn't. It {i/struck} me. I believed everything he said next, no matter how fantastic. The words were only flavoring on the truth in that stare. {pp} “There's a room,” he began, voice graveling, “not much farther down from here. Different from anything up here. Bigger. A bit bigger.” That laugh again."]
+	choices["Ch14Pathway"] = ["Ch14Pathway", 14, "I flailed, but my body was already past the edge of the door, my hands too slow to grab the frame, world tilting at a sickening angle. My sneaker tried desperately to glue itself to the carpet of the hall but my center of mass was too far out, way too far. My head dropped below my feet and I opened my mouth to scream as I began to fall into nothingness."]
+
+# [DEFINE 50>@spiralhall|50>@nikofalls]
+# [DEFINE 34>@noch8extra|33>@hamsterwheel|33>@stageladder]
 
 	candidates = choices.keys()
 	
@@ -137,6 +146,13 @@ def getSequenceToRender(parseParams):
 	if variables.check("bradphone"):
 		# This is only an interesting choice if dadphone was set (the other version of the party conversation is much shorter).
 		candidates.remove("Ch3PartyConvo")
+	if variables.check("spiralhall"):
+		candidates.remove("Ch8SpiralHall")
+	if variables.check("hamsterwheel"):
+		candidates.remove("Ch8HamsterWheel")
+	if variables.check("stageladder"):
+		candidates.remove("Ch8StageLadder")
+
 
 	choice = chooser.oneOf(candidates)
 
@@ -146,6 +162,26 @@ def getSequenceToRender(parseParams):
 		variables.__v.shuffleGroupVal("dadphone")
 	elif choice == "Ch4Interlude":
 		variables.__v.shuffleGroupVal("cdrom")
+	elif choice == "Ch6ResearchResults":
+		variables.__v.shuffleGroupVal("vortex")
+	elif choice == "Ch8SpiralHall":
+		variables.set("spiralhall", True)
+		variables.set("nikofalls", False)
+	elif choice == "Ch8HamsterWheel":
+		variables.set("hamsterwheel", True)
+		variables.set("noch8extra", False)
+		variables.set("stageladder", False)
+	elif choice == "Ch8StageLadder":
+		variables.set("stageladder", True)
+		variables.set("noch8extra", False)
+		variables.set("hamsterwheel", False)
+	elif choice == "Ch11TheBottom":
+		variables.__v.shuffleGroupVal("thecity")
+	elif choice == "Ch14Pathway":
+		variables.__v.shuffleGroupVal("ffchandelier")
+
+
+
 
 	return choices[choice]
 
