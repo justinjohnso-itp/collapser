3f4e07a2b5eac13e126aaef4606ebf9262240376
Aaron Reed <aareed@gmail.com>
2020-01-29 11:53:22 -0800

    Refinements for working with multiple accounts

--- collapser/twitter.py
+++ collapser/twitter.py
@@ -6,24 +6,37 @@ import fileio
 creds = {}
 
 def getLastTweet(account):
-	global creds
-	setCredentials()
-	twitter = Twython(creds["APP_KEY"], creds["APP_SECRET"], creds["OAUTH_ACCESS_TOKEN"], creds["OAUTH_ACCESS_TOKEN_SECRET"])
-	user_timeline = twitter.get_user_timeline(user_id=account)
+	twitter = getTwitter(account)
+	user_timeline = twitter.get_user_timeline()
 	if len(user_timeline) > 0:
 		lastTweet = user_timeline[0]
 		return lastTweet["text"]
 
-	# auth = twitter.get_authentication_tokens()
+def verifyCredentials(account):
+	twitter = getTwitter(account)
+	return twitter.verify_credentials()
 
-def setCredentials():
+def tweet(account, msg):
+	twitter = getTwitter(account)
+	response = twitter.update_status(status=msg)
+	print response
+	# id = response["id"]
+
+
+
+def getTwitter(account):
 	global creds
-	if "APP_KEY" not in creds:
-		credsRaw = fileio.readInputFile("collapser/keys/twitter.keys")
-		credsLines = credsRaw.split("\n")
-		creds = {
-			"APP_KEY": credsLines[0],
-			"APP_SECRET": credsLines[1],
-			"OAUTH_ACCESS_TOKEN": credsLines[2],
-			"OAUTH_ACCESS_TOKEN_SECRET": credsLines[3]
-		}
+	setCredentials(account)
+	return Twython(creds["APP_KEY"], creds["APP_SECRET"], creds["OAUTH_ACCESS_TOKEN"], creds["OAUTH_ACCESS_TOKEN_SECRET"])
+
+def setCredentials(account):
+	global creds
+	# if "APP_KEY" not in creds:
+	credsRaw = fileio.readInputFile("collapser/keys/tw.%s.keys" % account)
+	credsLines = credsRaw.split("\n")
+	creds = {
+		"APP_KEY": credsLines[0],
+		"APP_SECRET": credsLines[1],
+		"OAUTH_ACCESS_TOKEN": credsLines[2],
+		"OAUTH_ACCESS_TOKEN_SECRET": credsLines[3]
+	}
